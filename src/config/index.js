ff7639d086de29513097408a12756923e0c1a581
tysonrm <trmidboe@gmail.com>
2022-12-12 20:42:49 -0600

    doc

--- src/config/index.js
+++ src/config/index.js
@@ -1,1 +1,7 @@
-// this dir contains the ModelSpecifcation for each domain model. The implementation is in the domain folder.
+/**
+ * this dir contains the ModelSpecifcation for each domain model. The implementation is in the domain folder.
+ * export your model config to deploy,e.g.
+ * ```js
+ * export * from './MyDomainModel'
+ * ```
+ */
